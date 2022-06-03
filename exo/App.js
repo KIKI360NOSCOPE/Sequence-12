2997d4bae7f0a59128e7a6018b54b936142a5e85
mglehavre <mglehavre@live.fr>
2022-06-03 11:30:52 +0200

    stack navigator ne marche pas

--- exo/App.js
+++ exo/App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import TabNavigator from '../exo/src/Navigation/TabsNavigator';
+import TabNavigator from './src/Navigation/TabsNavigator';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import HeaderComponent from './src/Components/HeaderComponent';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <PaperProvider style={{ flex: 1 }}>
-        <View style={styles.container}>
+        <View style={{flex: 1}} >
+          <HeaderComponent />
           <TabNavigator />
-          <StatusBar />
+          <StatusBar style="auto" />
         </View>
       </PaperProvider>
     </SafeAreaProvider>
-
   );
 }
 
@@ -23,6 +23,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    marginTop: 40,
   },
 });
--- exo/src/Screens/CompteScreen.js
+++ exo/src/Screens/CompteScreen.js
@@ -1,12 +1,45 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, Picker, DatePickerIOS, TouchableOpacity, Alert, Image } from 'react-native';
 
-const CompteScreen = () => {
+const CompteScreen = ({ navigation }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>CompteScreen</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Gestion')}>
+          <Text style={styles.buttonText}>Se connecter</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
-export default CompteScreen
+export default CompteScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#00bfff',
+    padding: 10,
+    borderRadius: 5,
+    width: '40%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
+})
+
